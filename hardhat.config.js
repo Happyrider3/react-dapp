00277a02fd10e7735a1b75eecc75639f2de5d50f
Happyrider3 <riderhappy05@gmail.com>
2022-05-31 08:58:52 -0400

    update deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,13 +17,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  defaultNetwork: "hardhat",
   paths: {
     artifacts: './src/artifacts',
   },
   networks: {
     hardhat: {
-      chainId: 1337
+      //chainId: 1337
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/52d79b13b9b64bf68a88d4b74707070d",
+      accounts: [`0x1b50e9c54f7d311fb3747da6893155998f1e9d1e92a026d5f69ed7f83cb301b2`]
     }
-  }
+  },
+  solidity: "0.8.4",
 };
+
